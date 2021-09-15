55c4996b8d6eb49dc20f4778afda189445e76a36
szydab1 <up814818@myport.ac.uk>
2021-09-15 23:39:08 +0100

    Adds show_list command. Updated help. Updated loop with new command. When DONE now prints the list. Also cleaned up code and handling errors.

--- shopping_list.py
+++ shopping_list.py
@@ -1,5 +1,15 @@
 shopping_list = []
 
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+To add an item. Enter what you need after the '==>' sign.
+Enter "HELP" to show help.
+Enter "SHOW" to see your current list
+Enter "DONE" to stop adding items.
+""")
+
+
 def add_to_list(item):
     shopping_list.append(item)
     num_of_items = len(shopping_list)
@@ -10,12 +20,13 @@ def add_to_list(item):
         print("The item: '{}' has been added! There are currently {} items in the list."
         .format(item, num_of_items))
 
-def show_help():
-    print("What should we pick up at the store?")
-    print("""
-Enter "DONE" to stop adding items.
-Enter "HELP" to show help.
-""")
+
+def show_list():
+    current_list = shopping_list.copy()
+    current_shopping_list = ", ".join(current_list)
+    current_shopping_list = current_shopping_list.title()
+    print("Here's your list: {}".format(current_shopping_list))
+
 
 show_help()
 while True:
@@ -25,10 +36,13 @@ while True:
     elif new_item.upper() == "HELP":
         show_help()
         continue
+    elif new_item.upper() == "SHOW":
+        show_list()
+        continue
     else:
         add_to_list(new_item)
 
-#final_list = shopping_list.copy()
-#final_shopping_list = ", ".join(final_list)
-#final_shopping_list = final_shopping_list.title()
-#print(final_shopping_list)
+final_list = shopping_list.copy()
+final_shopping_list = ", ".join(final_list)
+final_shopping_list = final_shopping_list.title()
+print("Here's your completed list:",final_shopping_list)

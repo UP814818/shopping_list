37bcd23ea85e18945579e15428bcb244dc90e070
szydab1 <up814818@myport.ac.uk>
2021-09-15 23:06:09 +0100

    Adds ability to add itmes to list & upon adding an item prints total lenght of list

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,15 @@
+shopping_list = []
+
+def add_to_list(item):
+    shopping_list.append(item)
+    num_of_items = len(shopping_list)
+    if num_of_items == 1:
+        print("The item: '{}' has been added! There is currently {} item in the list."
+            .format(item, num_of_items))
+    else:
+        print("The item: '{}' has been added! There are currently {} items in the list."
+        .format(item, num_of_items))
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
@@ -13,3 +25,10 @@ while True:
     elif new_item.upper() == "HELP":
         show_help()
         continue
+    else:
+        add_to_list(new_item)
+
+#final_list = shopping_list.copy()
+#final_shopping_list = ", ".join(final_list)
+#final_shopping_list = final_shopping_list.title()
+#print(final_shopping_list)
